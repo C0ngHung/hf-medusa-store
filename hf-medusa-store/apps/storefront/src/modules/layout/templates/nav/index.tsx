8336e87d27d3e81d566a7b285b8e982005bd1dbf
linh-ca-nhan <hailinh2910203@gmail.com>
2026-07-13 18:51:20 +0700

    Refactor code structure for improved readability and maintainability

--- hf-medusa-store/apps/storefront/src/modules/layout/templates/nav/index.tsx
+++ hf-medusa-store/apps/storefront/src/modules/layout/templates/nav/index.tsx
@@ -6,6 +6,7 @@ import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
+import SearchBar from "@modules/layout/components/search-bar"
 import SideMenu from "@modules/layout/components/side-menu"
 
 export default async function Nav() {
@@ -21,7 +22,11 @@ export default async function Nav() {
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
-              <SideMenu regions={regions} locales={locales} currentLocale={currentLocale} />
+              <SideMenu
+                regions={regions}
+                locales={locales}
+                currentLocale={currentLocale}
+              />
             </div>
           </div>
 
@@ -37,6 +42,7 @@ export default async function Nav() {
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
+              <SearchBar />
               <LocalizedClientLink
                 className="hover:text-ui-fg-base"
                 href="/account"
